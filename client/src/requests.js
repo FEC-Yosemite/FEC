90762d8eb8a01979238632885d07e8cecea33ec8
Edward Cramer <eddiecramer2000@gmail.com>
2021-06-03 16:08:06 -0500

    Created product request functions

--- client/src/requests.js
+++ client/src/requests.js
@@ -1,6 +1,8 @@
 import { api } from '../../config.js';
 const axios = require('axios');
 
+// Products Api
+
 export function getProducts(page, count) {
   var options = {
     method: 'get',
@@ -14,3 +16,40 @@ export function getProducts(page, count) {
 
   return axios(options);
 }
+
+export function getProductById(id) {
+  var options = {
+    method: 'get',
+    url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products/${id}`,
+    headers: {
+      authorization: api
+    }
+  }
+
+  return axios(options);
+}
+
+export function getProductStyles(id) {
+  var options = {
+    method: 'get',
+    url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products/${id}/styles`,
+    headers: {
+      authorization: api
+    }
+  }
+
+  return axios(options);
+}
+
+export function getRelatedProducts(id) {
+  var options = {
+    method: 'get',
+    url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products/${id}/related`,
+    headers: {
+      authorization: api
+    }
+  }
+
+  return axios(options);
+}
+
