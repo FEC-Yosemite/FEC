fa39b9e8f6a8dc68f83e9ce35d5568e2806a5d99
root <agaillard88@gmail.com>
2021-06-03 18:19:28 -0400

    Connected stylesheet and added some additional elements in overview

--- client/src/components/overview/components/Gallery.jsx
+++ client/src/components/overview/components/Gallery.jsx
@@ -8,7 +8,9 @@ class Gallery extends React.Component {
 
   render() {
     return(
-      <div id="Gallery">Gallery</div>
+      <div id="Gallery">
+        <img src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"></img>
+      </div>
     )
   }
 }
