77fa17a559be818cf7c18de3846ad3824e83b4f0
root <agaillard88@gmail.com>
2021-06-04 13:50:08 -0400

    Added SASS and seperate stylesheets

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app.jsx';
-import '../../client/dist/styles/style.css';
+import '../../client/dist/styles/style.scss';
 
 ReactDOM.render(
   <App />,
