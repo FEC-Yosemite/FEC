5cd25d1107645b5adcb5d5ea2f9fc6482ca24a5d
Matt Jennings <mjenningscba@gmail.com>
2021-06-16 18:03:16 -0400

    Finished styling RelatedItems

--- client/src/components/relatedItems/Carousel.jsx
+++ client/src/components/relatedItems/Carousel.jsx
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import RelatedProductCard from './RelatedProductCard.jsx';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 
 class Carousel extends Component {
     constructor(props) {
@@ -68,7 +68,7 @@ class Carousel extends Component {
                 this.state.page === 0 ?
                 <div id="left-button-placeholder"> </div>
                 :
-                <FontAwesomeIcon id="left-button" onClick={this.buttonClick} icon={faArrowLeft} />
+                <FontAwesomeIcon id="left-button" onClick={this.buttonClick} icon={faChevronLeft} />
                 }
             </div>
                 {
@@ -85,7 +85,7 @@ class Carousel extends Component {
                 (this.state.window_size * this.state.page) + (this.state.window_size) >= this.props.outfits_list.length ?
                 <div id="right-button-placeholder"> </div>
                 :
-                <FontAwesomeIcon id="right-button" onClick={this.buttonClick} icon={faArrowRight} />
+                <FontAwesomeIcon id="right-button" onClick={this.buttonClick} icon={faChevronRight} />
                 }
             </div>
             </div>
